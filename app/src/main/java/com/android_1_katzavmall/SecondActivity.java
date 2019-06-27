9042b21c1d6e0149b3bf0a1d8e6a0f342b6c9bac
Melinda <mel.levi7@gmail.com>
2019-06-27 14:15:43 +0300

    test

---
+++ app/src/main/java/com/android_1_katzavmall/SecondActivity.java
@@ -0,0 +1,10 @@
+package com.android_1_katzavmall;
+
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+}
