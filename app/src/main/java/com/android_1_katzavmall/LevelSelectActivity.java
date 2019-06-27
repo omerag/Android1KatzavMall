9c241ae7f6a9f4a2ec248f4171d663712191fa16
omerag <44685338+omerag@users.noreply.github.com>
2019-06-27 15:10:38 +0300

    1
    1

--- app/src/main/java/com/android_1_katzavmall/LevelSelectActivity.java
+++ app/src/main/java/com/android_1_katzavmall/LevelSelectActivity.java
@@ -5,14 +5,14 @@ import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SecondActivity extends AppCompatActivity {
+public class LevelSelectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
-        setContentView(R.layout.second_activity);
+        setContentView(R.layout.level_select_activity);
 
     }
 }
