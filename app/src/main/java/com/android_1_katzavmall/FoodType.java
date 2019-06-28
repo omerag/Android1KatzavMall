209e52104b2f7e7f953905fc655b968bfcd47747
omerag <bamba2me>
2019-06-28 20:48:18 +0300

    FoodType is now complete

--- app/src/main/java/com/android_1_katzavmall/FoodType.java
+++ app/src/main/java/com/android_1_katzavmall/FoodType.java
@@ -1,9 +1,58 @@
 package com.android_1_katzavmall;
 
 public enum FoodType {
-    food1,
-    food2,
-    food3,
-    food4,
+
+    //Milk products
+    MILK,
+    WHITE_CHEESE,
+    HARD_CHEESE,
+    CHOCOLATE,
+    DANI,
+
+    //Meat products
+    STEAK,
+    CHICKEN,
+    SAUSAGE,
+    HAMBURGER,
+    SCHNITZEL,
+
+    //Vegan products
+    WATERMELON,
+    EGGPLANT,
+    BROCOLI,
+    ORANGE,
+    AVOKADO,
+
+    //Bread products
+    BAGUETTE,
+    DONUTS,
+    CROISSANT,
+    BREAD,
+    BAGEL,
+
+    //Morning products
+    EGGS,
+    PANCAKES,
+    TOMATO,
+
+    //Lunch products
+    POTATO,
+    CUCAMBER,
+    BEER,
+
+    //Rosh Hashana products
+    FISH,
+    POMEGRANATE,
+    HONEY,
+    APPLE,
+    CHALLAH,
+    WINE,
+    CARROT,
+
+    //Pesah products
+    MATZA,
+    CHOCOLATE_SPREAD,
+    BEETS,
+
 
 }
