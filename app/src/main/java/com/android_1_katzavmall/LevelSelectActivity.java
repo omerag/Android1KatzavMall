d5f5b77ace5dbd631ddcd2dbcd33632d3916a3b1
Melinda <mel.levi7@gmail.com>
2019-06-29 13:10:17 +0300

    Mel

--- app/src/main/java/com/android_1_katzavmall/LevelSelectActivity.java
+++ app/src/main/java/com/android_1_katzavmall/LevelSelectActivity.java
@@ -1,11 +1,16 @@
 package com.android_1_katzavmall;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LevelSelectActivity extends AppCompatActivity {
+
+    LinearLayout milk_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,16 @@ public class LevelSelectActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.level_select_activity);
+
+        milk_layout = findViewById(R.id.milk_layout);
+
+        milk_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent levelIntent = new Intent(LevelSelectActivity.this, GameActivity.class);
+                startActivity(levelIntent);
+            }
+        });
 
     }
 }
